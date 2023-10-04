2b67104c6b2f0b17d49b085ce196b539f5e0d63b
Thienan233 <an0906946088@gmail.com>
2023-10-04 13:56:46 +0700

    modify

--- quad_cnn_env_no_contact.py
+++ quad_cnn_env_no_contact.py
@@ -259,7 +259,7 @@ class Quadrup_env():
     
     
     def truncation_check(self,ori,dir,robotId):
-        return  (self.time_steps_in_current_episode[robotId]>self.max_length) | (dir<.8) | (ori<.8) | (np.abs(self.base_pos[robotId,1]) > (.25*self.terrain_shape[-1]/self.num_robot) )
+        return  (self.time_steps_in_current_episode[robotId]>self.max_length) | (ori<.5) | (np.abs(self.base_pos[robotId,1]) > (.25*self.terrain_shape[-1]/self.num_robot) )
     
     
     def auto_reset(self,robotId):
@@ -379,23 +379,23 @@ class Quadrup_env():
         return [speed, align, high, surv, force,  contact]
     
 # # # TEST CODE # # #
-# env = Quadrup_env(
-#                     render_mode     = 'human',
-#                     num_robot       = 1,
-#                     debug           = True,
-#                     terrainHeight   = [0. ,0.05],
-#                     )             
-# for time in range(1000):
-#     # print(env.time_steps_in_current_episode)
-#     action = env.get_run_gait(env.time_steps_in_current_episode)
-#     # action = np.random.uniform(-.1,.1,(env.num_robot,env.number_of_joints))
-#     obs, rew, inf = env.sim(action,real_time=True)
-#     # t.sleep(1./240.)
-#     # t.sleep(.5)
-#     # print(obs.shape,rew.shape,inf.shape)
-#     # print(env.time_steps_in_current_episode)
-#     # print(time,obs[0])
-#     # print(rew[0])
-#     # print(inf[0])
-#     # print('-'*100)
-# env.close()
+env = Quadrup_env(
+                    render_mode     = 'human',
+                    num_robot       = 2,
+                    debug           = True,
+                    terrainHeight   = [0. ,0.05],
+                    )             
+for time in range(1000):
+    # print(env.time_steps_in_current_episode)
+    action = env.get_run_gait(env.time_steps_in_current_episode)
+    # action = np.random.uniform(-.1,.1,(env.num_robot,env.number_of_joints))
+    obs, rew, inf = env.sim(action,real_time=True)
+    # t.sleep(1./240.)
+    # t.sleep(.5)
+    # print(obs.shape,rew.shape,inf.shape)
+    # print(env.time_steps_in_current_episode)
+    # print(time,obs[0])
+    # print(rew[0])
+    # print(inf[0])
+    # print('-'*100)
+env.close()
